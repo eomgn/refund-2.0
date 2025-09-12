81d29852e16e65df722f6502140271561be1e28a
eomgn <matheeusgn21@gmail.com>
2025-09-12 01:55:56 -0300

    :tada: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold text-green-500 text-xxs">
+          Refund App
+        </h1>
+      </div>
+    </>
+  );
+}
