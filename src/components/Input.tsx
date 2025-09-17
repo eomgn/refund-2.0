8ebfa73e502e5efc316822517d62b4cc16d41051
eomgn <matheeusgn21@gmail.com>
2025-09-17 05:25:08 -0300

    :sparkles: feat: criado o component para button e ajustado type do input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,7 +2,7 @@ type Props = React.ComponentProps<"input"> & {
   legend?: string;
 };
 
-export function Input({ legend, ...rest }: Props) {
+export function Input({ legend, type = "text", ...rest }: Props) {
   return (
     <>
       <fieldset className="flex flex-1 max-h-20 text-gray-200  focus-within:text-green-100">
@@ -13,6 +13,7 @@ export function Input({ legend, ...rest }: Props) {
         )}
 
         <input
+          type={type}
           {...rest}
           className="w-full h-12 rounded-lg border border-gray-300 px-4 text-sm text-gray-100 bg-transparent outline-none focus:border-2 focus:border-green-100 placeholder-gray-300"
         />
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,5 @@
 import { Input } from "../components/Input";
+import { Button } from "../components/Button";
 
 export function SignIn() {
   return (
@@ -11,6 +12,8 @@ export function SignIn() {
       />
 
       <Input type="password" required legend="Senha" placeholder="123456" />
+
+      <Button isLoading>Entrar</Button>
     </form>
   );
 }
