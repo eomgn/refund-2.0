9c9c63feff3a9082a08442edcc3fe65450d581b3
eomgn <matheeusgn21@gmail.com>
2025-09-18 14:17:55 -0300

    :sparkles: feat: estilizando componente Refund

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,7 +1,20 @@
+import { Input } from "../components/Input";
+
 export function Refund() {
   return (
     <>
-      <h1>Refund</h1>
+      <form className="bg-gray-500 flex flex-col rounded-lg lg:w-min[512px] p-10 gap-6">
+        <header>
+          <h1 className="font-bold text-xl text-gray-100">
+            Solicitação de reembolso
+          </h1>
+          <p className="text-gray-200 text-sm mt-3 mb-10">
+            Dados da despesa para solicitar reembolso
+          </p>
+        </header>
+
+        <Input required legend="Nome da solicitação" />
+      </form>
     </>
   );
 }
