71eea82b49af028a5823be7053bb3508dd2c9f83
eomgn <matheeusgn21@gmail.com>
2025-09-19 23:32:14 -0300

    :sparkles: feat: estilizado o Confirm

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -1,5 +1,7 @@
 import { Navigate, useLocation } from "react-router";
 
+import okSvg from "../assets/ok.svg";
+
 export function Confirm() {
   const location = useLocation();
 
@@ -9,7 +11,25 @@ export function Confirm() {
 
   return (
     <>
-      <h1>Confirm</h1>
+      <div className="bg-gray-500 lg:w-[512px] rounded-xl flex flex-col items-center p-10 gap-6">
+        <h1 className="text-green-100 text-2xl font-bold text-center">
+          Solicitação enviada!
+        </h1>
+
+        <img src={okSvg} alt="Ícone de Ok" className="w-32" />
+
+        <p className="w-full text-sm text-gray-200 text-center">
+          Agora é apenas aguardar! Sua solicitação será analisada e, em breve, o
+          setor financeiro irá entrar em contato com você.
+        </p>
+
+        <a
+          href="/"
+          className="w-full bg-green-100 text-center text-white p-3 rounded-lg hover:bg-green-200 transition ease-linear"
+        >
+          Nova solicitação
+        </a>
+      </div>
     </>
   );
 }
