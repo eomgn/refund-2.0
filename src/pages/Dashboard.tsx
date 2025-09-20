3f36584393046ce3a53917a45f28c92dc674c07e
eomgn <matheeusgn21@gmail.com>
2025-09-20 14:15:32 -0300

    :sparkles: feat: criado o input para pesquisar pelo nome em dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,30 @@
+import { useState, type FormEvent } from "react";
+import { Input } from "../components/Input";
+
 export function Dashboard() {
+  const [name, setName] = useState("");
+
+  function fectchRefunds(e: FormEvent) {
+    e.preventDefault();
+
+    console.log(name);
+  }
+
   return (
     <>
-      <h1>Dashboard</h1>
+      <div className="bg-gray-500 p-10 rounded-lg text-gray-100 md:min-w-[768px]">
+        <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
+
+        <form
+          onSubmit={fectchRefunds}
+          className="flex flex-1 item-center justify-center pb-12 mt-6 gap-3 border-b-[1px] border-b-gray-400 "
+        >
+          <Input
+            placeholder="Pesquisar pelo nome"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </form>
+      </div>
     </>
   );
 }
