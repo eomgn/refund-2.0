303ed11cab241f92022885a808cfd8576fd6e1f6
eomgn <matheeusgn21@gmail.com>
2025-09-21 00:12:00 -0300

    :sparkles: feat: criado o componente de Loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,24 @@
 import { BrowserRouter } from "react-router";
 
+import { Loading } from "../components/Loading";
+
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 
+const isLoading = true;
+
 export function IndexRoutes() {
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <>
       <BrowserRouter>
-        {/* <AuthRoutes /> */}
+        <AuthRoutes />
         {/* <EmployeeRoutes /> */}
-        <ManagerRoutes />
+        {/* <ManagerRoutes /> */}
       </BrowserRouter>
     </>
   );
